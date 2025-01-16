08d504098a38d6714b0603199423ad32f516b639
Elena Kolesnikova <koleslena@yandex.ru>
2025-01-16 19:08:14 +0300

    fix next

--- index.py
+++ index.py
@@ -13,26 +13,30 @@ def poety_response(event):
     resp['text'] = read_file() + resp['text']
     return response_construct(event, resp)
 
+def check_in(text, lst):
+    txt = text.replace(',', '').replace('.', '').replace('!', '').replace('?', '')
+    return sum(1 for y in txt.split() if y in lst) > 0
+
 def handler(event, context):
 
     if 'request' in event and \
             'original_utterance' in event['request'] \
             and len(event['request']['original_utterance']) > 0:
         req_text = event['request']['original_utterance'].lower()
-        # TODO: common method
+        
         if req_text in exit_requests:
             return response_construct(event, exit_response)
         
-        if req_text in yes_request:
+        if check_in(req_text, yes_request):
             return poety_response(event)
         
-        if req_text in no_request:
+        if check_in(req_text, no_request):
             return response_construct(event, welcome_no_response)
         
-        if req_text in next_request:
+        if check_in(req_text, next_request):
             return poety_response(event)
         
-        if req_text in other_request:
+        if check_in(req_text, other_request):
             return poety_response(event)
         
         return response_construct(event, no_way_response)
